41bbcace30961c6be2e0dadc83b8dd46de2c26b3
Javier <javiermoralesr95@gmail.com>
2020-12-18 15:06:28 -0300

    Improved blending method.

--- stitch.py
+++ stitch.py
@@ -13,13 +13,26 @@ def extend_image(img1, img2):
             extended_img1[i_start+i][j] = img1[i][j]
     return extended_img1
 
+def one_empty_pixel(img1, img2):
+    selected = np.zeros(img1.shape, dtype=np.bool)
+    for i in range(img1.shape[0]):
+        for j in range(img1.shape[1]):
+            selected[i][j] = [empty_pixel(img1[i][j]) != empty_pixel(img2[i][j])] * 3
+    return selected
+
+def empty_pixel(pixel):
+    return pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0
+
 def stitch(img1, img2):
-    stitched = np.maximum(img1, img2)
+    where_max = one_empty_pixel(img1, img2)
+    stitched = 0.5 * img1 + 0.5 * img2
+    stitched = stitched.astype(np.uint8)
+    stitched = np.where(where_max, np.maximum(img1, img2), stitched)
     min_i = None
     for i in range(stitched.shape[0]):
         if not min_i:
             for j in range(stitched.shape[1]):
-                if stitched[i][j][0] > 0 or stitched[i][j][1] > 0 or stitched[i][j][2] > 0:
+                if not empty_pixel(stitched[i][j]):
                     min_i = i
                     break
         else:
@@ -28,7 +41,7 @@ def stitch(img1, img2):
     for i in reversed(range(stitched.shape[0])):
         if not max_i:
             for j in range(stitched.shape[1]):
-                if stitched[i][j][0] > 0 or stitched[i][j][1] > 0 or stitched[i][j][2] > 0:
+                if not empty_pixel(stitched[i][j]):
                     max_i = i
                     break
         else:
@@ -37,15 +50,15 @@ def stitch(img1, img2):
     for j in reversed(range(stitched.shape[1])):
         if not max_j:
             for i in range(stitched.shape[0]):
-                if stitched[i][j][0] > 0 or stitched[i][j][1] > 0 or stitched[i][j][2] > 0:
+                if not empty_pixel(stitched[i][j]):
                     max_j = j
                     break
     return stitched[min_i:max_i, 0:max_j, :]
 
 
 
-img1 = cv2.imread('./images/caso_2/2a.jpg', cv2.IMREAD_COLOR)
-img2 = cv2.imread('./images/caso_2/2b.jpg', cv2.IMREAD_COLOR)
+img1 = cv2.imread('./images/caso_1/1a.jpg', cv2.IMREAD_COLOR)
+img2 = cv2.imread('./images/caso_1/1b.jpg', cv2.IMREAD_COLOR)
 img1_ext = extend_image(img1, img2)
 img1 = img1_ext[:,0:img1.shape[1],:]
 
